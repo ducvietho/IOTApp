1660fcd3f311a6775fcc4277d5051a6269458f88
ducvietho <ducvietho1995@gmail.com>
2018-03-08 20:59:17 +0700

    alarm

--- app/src/main/java/com/example/ducvietho/iotapp/util/SaveDayRepeat.java
+++
@@ -1,9 +0,0 @@
-package com.example.ducvietho.iotapp.util;
-
-/**
- * Created by ducvietho on 3/5/2018.
- */
-
-public interface SaveDayRepeat<T> {
-    public void saveDayRepeat(T object,int id);
-}
